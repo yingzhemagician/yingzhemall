bafe2f56dd4d2aa3c8296f9a957d2cbc4cacfd6a
yingzhemagician <yingzhemagician@infermotion.com>
2019-07-31 01:03:08 -0700

    project init finished commit

---
+++ src/main/java/YingzheGroup/yingzhemall/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package YingzheGroup.yingzhemall.dao;
+
+import YingzheGroup.yingzhemall.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
