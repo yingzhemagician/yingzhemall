8098688c73a15c5ccffc31949f5935ec917e5266
yingzhemagician <yingzhemagician@infermotion.com>
2019-08-08 23:34:51 -0700

    Completed category module and test

--- src/main/java/YingzheGroup/yingzhemall/service/impl/CategoryServiceImpl.java
+++ src/main/java/YingzheGroup/yingzhemall/service/impl/CategoryServiceImpl.java
@@ -4,13 +4,23 @@ import YingzheGroup.yingzhemall.common.ServerResponse;
 import YingzheGroup.yingzhemall.dao.CategoryMapper;
 import YingzheGroup.yingzhemall.pojo.Category;
 import YingzheGroup.yingzhemall.service.ICategoryService;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.sun.tools.corba.se.idl.InterfaceGen;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.Set;
 
 @Service("CategoryService")
 public class CategoryServiceImpl implements ICategoryService {
 
+    private Logger logger = LoggerFactory.getLogger(CategoryServiceImpl.class);
     @Autowired
     private CategoryMapper categoryMapper;
 
@@ -46,5 +56,44 @@ public class CategoryServiceImpl implements ICategoryService {
             return ServerResponse.createBySuccess("Updated category name successfully.");
         }
         return ServerResponse.createByErrorMessage("Updated category name failed");
+    }
+
+    public ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId){
+        List<Category> categoryList = categoryMapper.selectCategoryChildrenByParentId(categoryId);
+        if(CollectionUtils.isEmpty(categoryList)){
+            logger.info("Cannot find the sub categories.");
+        }
+        return ServerResponse.createBySuccess(categoryList);
+    }
+
+    /**
+     * recursively retrieve the id and sub id
+     * @param categoryId
+     * @return YingzheGroup.yingzhemall.common.ServerResponse
+     */
+    public ServerResponse selectCategoryAndChildrenById(Integer categoryId){
+        Set<Category> categorySet = Sets.newHashSet();
+        findChildCategory(categorySet, categoryId);
+
+        List<Integer> categoryIdList = Lists.newArrayList();
+        if(categoryId != null){
+            for(Category categoryItem : categorySet){
+                categoryIdList.add(categoryItem.getId());
+            }
+        }
+        return ServerResponse.createBySuccess(categoryIdList);
+    }
+
+    private Set<Category> findChildCategory(Set<Category> categorySet, Integer categoryId){
+        Category category = categoryMapper.selectByPrimaryKey(categoryId);
+        if(category != null){
+            categorySet.add(category);
+        }
+
+        List<Category> categoryList = categoryMapper.selectCategoryChildrenByParentId(categoryId);
+        for(Category categoryItem : categoryList){
+            findChildCategory(categorySet, categoryItem.getId());
+        }
+        return categorySet;
     }
 }
